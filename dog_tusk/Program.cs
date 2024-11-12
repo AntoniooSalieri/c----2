dd86ece407585060b2725af38edc78b2631a48c7
AsuraVV <antonio.fon.salieri@gmail.com>
2024-11-12 11:23:46 +0200

    dog_tusk complite

---
+++ dog_tusk/Program.cs
@@ -0,0 +1,19 @@
+﻿using System.Buffers;
+
+int speed0 = 10,
+    speed1 = 3;
+int x = 0,          //позиция 1ого
+    y = 1000;       //позиция 2ого
+int distance = y;   //расстояние от 1 до 2
+int time = 0;       //время до встерчи
+int count = 0;      //кол-во встерч
+
+while (distance > 10){
+    distance = y - x;
+    time = distance / (speed0 + speed1);
+    x = x + time * speed1;
+    y = y - time * speed1;
+    count++;
+}
+
+Console.WriteLine(count);
