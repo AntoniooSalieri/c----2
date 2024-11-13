a68c9775ebcb94bb7219fff1162e597c3c3fe65f
AsuraVV <antonio.fon.salieri@gmail.com>
2024-11-13 12:13:09 +0200

    function file complite

--- functions/Program.cs
+++ functions/Program.cs
@@ -1,6 +1,16 @@
-﻿int x = 10, y = 1;
+﻿int x = 10, y = 9;
 int summ(int a, int b){
     int result = a + b;
     return result;
 }
+
+void Printer(int count){
+    while (count > 0){
+        Console.Write(count);
+        count--;
+    }
+}
+
+Printer(y);
+Console.WriteLine();
 Console.WriteLine(summ(x, y));
